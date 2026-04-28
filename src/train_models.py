cd745f493e689b30d8bb29f74befe190e521bde4
pratiksha002 <020606pratiksha@gmail.com>
2026-04-28 18:02:42 +0530

    Added GridSearchCV

--- src/train_models.py
+++ src/train_models.py
@@ -3,7 +3,7 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import GridSearchCV
 
-def train_model():
+def get_models_with_params():
     models = {
         "Linear Regression": {
             "model": LinearRegression(),
@@ -28,3 +28,26 @@ def train_model():
     }
 
     return models
+
+
+def train_models(x_train, y_train):
+    models = get_models_with_params()
+    trained_models = {}
+
+    for name, config in models.items():
+        model = config["model"]
+        params = config["params"]
+
+        grid = GridSearchCV(
+            model,
+            params,
+            cv=5,
+            scoring="neg_mean_absolute_error",
+            n_jobs=-1
+        )
+
+        grid.fit(x_train, y_train)
+        trained_models[name] = grid.best_estimator_
+        print(f"{name} trained with best params: {grid.best_params_}")
+
+    return trained_models
