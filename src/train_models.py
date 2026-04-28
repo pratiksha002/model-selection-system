02df6e65f087df6f1d57e416cad6857d09ff5d8c
pratiksha002 <020606pratiksha@gmail.com>
2026-04-28 13:27:22 +0530

    Added GridSearchCV

--- src/train_models.py
+++ src/train_models.py
@@ -1,12 +1,30 @@
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
 
 def train_model():
     models = {
-        "Linear Regression": LogisticRegression(),
-        "Decision Tree": DecisionTreeRegressor(max_depth=5, min_samples_split=4),
-        "Random Forest": RandomForestRegressor(n_estimators=100, max_depth=5, random_state=45)
+        "Linear Regression": {
+            "model": LinearRegression(),
+            "params":{}
+        },
+
+        "Decision Tree": {
+            "model": DecisionTreeRegressor(),
+            "params": {
+                "max_depth": [3, 5, 10],
+                "min_samples_split": [2, 5]
+            }
+        },
+
+        "Random Forest": {
+            "model": RandomForestRegressor(),
+            "params": {
+                "n_estimators": [50, 100],
+                "max_depth": [3, 5]
+            }
+        }
     }
 
     return models
